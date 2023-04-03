ce4a101244f01457cdb10cfdd25ad9bbe658ad53
Logan <loganc47sac@gmail.com>
2023-04-03 14:37:01 -0500

    m

--- roomadventure.py
+++ roomadventure.py
@@ -4,8 +4,46 @@
 
 from tkinter import * 
 
+
+
 class Room:
-    pass
+    """A room has a name and a filepath that points to a .gif image."""
+
+    def __init__(self, name:str, imageFilepath:str) -> None:
+        self.name = name
+        self.image = imageFilepath
+        self.exits = {} 
+        self.items = {}
+        self.grabbables = [] 
+
+    def addExit(self, label:str, room:'Room'): 
+        self.exits[label] = room
+    
+    def addItem(self, label:str, desc:str):
+        self.items[label] = desc 
+
+    def addGrabs(self, grab:str):
+        self.grabbables.append(grab)
+
+    def delGrab(self, grab:str):
+        self.grabbables.remove(grab)
+    
+    def __str__(self) -> str:
+        result = f'You are in {self.name}\n'
+
+        result += 'You see: '
+        for item in self.items.keys():
+            result += item + ' '
+        result += '\n'
+
+        result += 'Exits: '
+        for exits in self.exits.keys():
+            resutl += exits + ' '
+        result += '\n'
+
+        return result
+        
+
 
 class Game:
     pass
